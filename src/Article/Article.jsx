3b73099eac8b7d4e32f9e214c0b5e08f70990301
001083544_yanlinliu <253939101@qq.com>
2020-10-04 23:31:40 -0700

    hw4

--- src/Article/Article.jsx
+++ src/Article/Article.jsx
@@ -4,7 +4,6 @@ import "./index.css";
 const Article = () => {
   return (
     <div class="contianer">
-      <meta name="selected-link" value="repo_source" data-pjax-transient></meta>
       <h2>The Statue of Liberty's torch heads to new museum </h2>
       <p>
         <div>
